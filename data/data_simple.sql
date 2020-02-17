f6d7e47cbb2bdeabfd8f3246be0f7b9ffc28dc31
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-02-17 20:51:22 +0100

    new installer and fixes

--- data/data_simple.sql
+++
@@ -1,5 +0,0 @@
---
--- Matching Partial
---
-INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
-(NULL, 'partial', 'Matching', 'matching', 'articlerequest-matching', 'articlerequest-single', 'col-md-12', '', '', 0, 1, 0, '', '', '');
